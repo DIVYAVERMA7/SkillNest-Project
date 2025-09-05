a4f85fd64216c44ea7ef8c6d2795dd722b72f280
DIVYAVERMA7 <125733958+DIVYAVERMA7@users.noreply.github.com>
2025-09-05 17:06:07 +0530

    course

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,26 @@
 const express=require('express')
 const courserouter = express.Router()
-const {Coursemodel} = require('../db')
+const {Purchasesmodel,Coursemodel} = require('../db')
+const { userMiddleware } = require('../middleware/user')
 
+courserouter.post('/purcahse',userMiddleware,async (req,res)=>{
+    const userId = req.userId
+    const courseId = req.body.courseId
 
+    await Purchasesmodel.create({
+        userId,courseId
+    })
 
+    res.json({
+        message:"you have successfully bought the course"
+    })
+})
 
-
-
+courserouter.get('/preview',async (req,res)=>{
+    const courses = await Coursemodel.find({})
+    res.json({
+        courses
+    })
+})
 
 module.exports=courserouter
